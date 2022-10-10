b4cc562d02776eb363db4096465f49a154cc733e
Ho Minh Duy Nguyen <nguyen@kiew.kl.dfki.de>
2022-10-11 00:40:00 +0200

    update script

--- OpenEA_JMAC/open_ea.sh
+++ OpenEA_JMAC/open_ea.sh
@@ -2,6 +2,6 @@ for version in V1 V2
 do 
 for dataname in D_W D_Y EN_DE EN_FR
 do 
-python main_with_args.py --training_data OpenEA_dataset_v1.1/${dataname}_15K_${version}
+python main_with_args.py --training_data OpenEA_dataset_v1.1/${dataname}_15K_${version}/
 done 
 done 
